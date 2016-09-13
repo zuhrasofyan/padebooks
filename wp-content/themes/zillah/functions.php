a840c79c33a8071a9336912d803c95f2f19263d0
Zuhra Sofyan <zuhrasofyan@gmail.com>
2016-09-13 21:37:29 +0700

    pb-002 change continue reading
    to lanjut membaca

--- wp-content/themes/zillah/functions.php
+++ wp-content/themes/zillah/functions.php
@@ -282,13 +282,13 @@ require get_template_directory() . '/inc/zillah_hooks.php';
 
 
 function zillah_read_more_link() {
-	return '<a href="'. esc_url( get_permalink(get_the_ID()) ) . '" class="more-link">' . sprintf( __( 'Continue Reading %s', 'zillah' ), the_title( '<span class="screen-reader-text">"', '"</span>', false ) . ' <span class="meta-nav">&rarr;</span>' ) . '</a>';
+	return '<a href="'. esc_url( get_permalink(get_the_ID()) ) . '" class="more-link">' . sprintf( __( 'Lanjut Membaca %s', 'zillah' ), the_title( '<span class="screen-reader-text">"', '"</span>', false ) . ' <span class="meta-nav">&rarr;</span>' ) . '</a>';
 }
 add_filter( 'the_content_more_link', 'zillah_read_more_link' );
 
 
 function zillah_excerpt_more() {
-	return '...<span class="clearfix clearfix-post"></span><a href="'. esc_url( get_permalink(get_the_ID()) ) . '" class="more-link">' . sprintf( __( 'Continue Reading %s', 'zillah' ), the_title( '<span class="screen-reader-text">"', '"</span>', false ) . ' <span class="meta-nav">&rarr;</span>' ) . '</a>';
+	return '...<span class="clearfix clearfix-post"></span><a href="'. esc_url( get_permalink(get_the_ID()) ) . '" class="more-link">' . sprintf( __( 'Lanjut Membaca %s', 'zillah' ), the_title( '<span class="screen-reader-text">"', '"</span>', false ) . ' <span class="meta-nav">&rarr;</span>' ) . '</a>';
 }
 add_filter('excerpt_more', 'zillah_excerpt_more');
 
@@ -524,14 +524,14 @@ function zillah_php_style() {
 			echo '
 				.post-navigation .nav-links a,
 				.posts-navigation .nav-previous a,
-				.posts-navigation .nav-previous a, 
+				.posts-navigation .nav-previous a,
 				.posts-navigation .nav-next a {
 					background: ' . $zillah_c1 . ';
 					opacity: 1;
 				}
 				.post-navigation .nav-links a:hover,
 				.posts-navigation .nav-previous a:hover,
-				.posts-navigation .nav-previous a:hover, 
+				.posts-navigation .nav-previous a:hover,
 				.posts-navigation .nav-next a:hover {
 					background: ' . $zillah_c1 . ';
 					opacity: 0.8;
@@ -588,10 +588,10 @@ function zillah_php_style() {
 		/* Color 3 */
 		if( !empty( $zillah_c3 ) ) {
 			echo '
-				 .main-navigation li:hover > a:hover, 
+				 .main-navigation li:hover > a:hover,
 				 .main-navigation li.focus > a:hover,
 				 .widget li a:hover,
-				 .main-navigation li:hover > a, 
+				 .main-navigation li:hover > a,
 				 .main-navigation li.focus > a,
 				 a.post-edit-link:hover,
 				 .tags-links a:hover,
@@ -620,8 +620,8 @@ function zillah_php_style() {
 						background:' . $zillah_c3 . ';
 					 }
 					 .dropdown-toggle,
-					 .dropdown-toggle.toggled-on, 
-					 .dropdown-toggle.toggled-on:hover, 
+					 .dropdown-toggle.toggled-on,
+					 .dropdown-toggle.toggled-on:hover,
 					 .dropdown-toggle.toggled-on:focus {
 						color: ' . $zillah_c3 . ';
 					 }
@@ -643,27 +643,27 @@ function zillah_php_style() {
 		if( !empty( $zillah_c5 ) ) {
 			echo '
 				body {
-					color: ' . $zillah_c5 . '; 
+					color: ' . $zillah_c5 . ';
 				}
 			';
 		}
 
 		echo '
-			.carousel-caption-title, 
+			.carousel-caption-title,
 			.carousel-caption-title a {
 				color: #373735;
 			}
-			
+
 			.widget-area .widget li a {
 				color: #6f6e6b;
 			}
 			@media screen and (max-width: 991px) {
-				.main-navigation ul ul li:hover > a:hover, 
-				.main-navigation ul ul li.focus > a:hover, 
-				.main-navigation ul ul li:hover > a, 
+				.main-navigation ul ul li:hover > a:hover,
+				.main-navigation ul ul li.focus > a:hover,
+				.main-navigation ul ul li:hover > a,
 				.main-navigation ul ul li.focus > a,
 				.main-navigation a:visited {
-					color: #FFF; 
+					color: #FFF;
 				}
 			}
 		';
@@ -894,7 +894,7 @@ function zillah_post_video() {
 		}
 		echo '</div>';
 	}
-	
+
 }
 
 
@@ -914,6 +914,4 @@ function zillah_themeisle_sdk(){
 	);
 }
 
-zillah_themeisle_sdk(); 
-
- 
+zillah_themeisle_sdk();
