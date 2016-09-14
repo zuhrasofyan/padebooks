85e9eb4645578874cfad6c8981f70a2dd4f53386
Zuhra Sofyan <zuhrasofyan@gmail.com>
2016-09-14 19:54:30 +0700

    pb-005 delete link to wordpress.org

--- wp-includes/widgets/class-wp-widget-meta.php
+++ wp-includes/widgets/class-wp-widget-meta.php
@@ -57,22 +57,6 @@ class WP_Widget_Meta extends WP_Widget {
 			<li><?php wp_loginout(); ?></li>
 			<li><a href="<?php echo esc_url( get_bloginfo( 'rss2_url' ) ); ?>"><?php _e('Entries <abbr title="Really Simple Syndication">RSS</abbr>'); ?></a></li>
 			<li><a href="<?php echo esc_url( get_bloginfo( 'comments_rss2_url' ) ); ?>"><?php _e('Comments <abbr title="Really Simple Syndication">RSS</abbr>'); ?></a></li>
-			<?php
-			/**
-			 * Filters the "Powered by WordPress" text in the Meta widget.
-			 *
-			 * @since 3.6.0
-			 *
-			 * @param string $title_text Default title text for the WordPress.org link.
-			 */
-			echo apply_filters( 'widget_meta_poweredby', sprintf( '<li><a href="%s" title="%s">%s</a></li>',
-				esc_url( __( 'https://wordpress.org/' ) ),
-				esc_attr__( 'Powered by WordPress, state-of-the-art semantic personal publishing platform.' ),
-				_x( 'WordPress.org', 'meta widget link text' )
-			) );
-
-			wp_meta();
-			?>
 			</ul>
 			<?php
 		echo $args['after_widget'];
